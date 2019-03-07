c609db2f8dea30bf4606c07fee2bfa2a2f432e23
Matt Edmonston <matt@ethercreative.co.uk>
2019-03-07 17:41:46 +0000

    Initial.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+PROXY_PATH=/cnt/proxy
+
+if [ ! -d /cnt ]
+then
+    mkdir /cnt
+fi
+
+git clone https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion.git $PROXY_PATH
+
+echo "NGINX_WEB=nginx-web" >> $PROXY_PATH/.env
+echo "DOCKER_GEN=nginx-gen" >> $PROXY_PATH/.env
+echo "LETS_ENCRYPT=nginx-letsencrypt" >> $PROXY_PATH/.env
+echo "IP=0.0.0.0" >> $PROXY_PATH/.env
+echo "NETWORK=proxy" >> $PROXY_PATH/.env
+echo "NGINX_FILES_PATH=./nginx-data" >> $PROXY_PATH/.env
+
+curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/docker-compose.override.yml >> $PROXY_PATH/docker-compose.override.yml
+curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/proxy.conf >> $PROXY_PATH/proxy.conf
+
+$PROXY_PATH/start.sh
