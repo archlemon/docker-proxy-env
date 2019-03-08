6b163592cb8d647f826503a8871b42dae0d7f13a
Matt Edmonston <matt@ethercreative.co.uk>
2019-03-08 12:52:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 PROXY_PATH=/cnt/proxy
+PWD=$(pwd)
 
 if [ ! -d /cnt ]
 then
@@ -9,14 +10,20 @@ fi
 
 git clone https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion.git $PROXY_PATH
 
-echo "NGINX_WEB=nginx-web" >> $PROXY_PATH/.env
-echo "DOCKER_GEN=nginx-gen" >> $PROXY_PATH/.env
-echo "LETS_ENCRYPT=nginx-letsencrypt" >> $PROXY_PATH/.env
-echo "IP=0.0.0.0" >> $PROXY_PATH/.env
-echo "NETWORK=proxy" >> $PROXY_PATH/.env
-echo "NGINX_FILES_PATH=./nginx-data" >> $PROXY_PATH/.env
+touch ${PROXY_PATH}/.env
 
-curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/docker-compose.override.yml >> $PROXY_PATH/docker-compose.override.yml
-curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/proxy.conf >> $PROXY_PATH/proxy.conf
+echo "NGINX_WEB=nginx-web" >> ${PROXY_PATH}/.env
+echo "DOCKER_GEN=nginx-gen" >> ${PROXY_PATH}/.env
+echo "LETS_ENCRYPT=nginx-letsencrypt" >> ${PROXY_PATH}/.env
+echo "IP=0.0.0.0" >> ${PROXY_PATH}/.env
+echo "NETWORK=proxy" >> ${PROXY_PATH}/.env
+echo "NGINX_FILES_PATH=./nginx-data" >> ${PROXY_PATH}/.env
 
-$PROXY_PATH/start.sh
+curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/docker-compose.override.yml >> ${PROXY_PATH}/docker-compose.override.yml
+curl https://raw.githubusercontent.com/archlemon/docker-proxy-env/master/proxy.conf >> ${PROXY_PATH}/proxy.conf
+
+cd $PROXY_PATH
+
+${PROXY_PATH}/start.sh
+
+cd $PWD
